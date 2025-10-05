8e9a2bbcc1a3c199db86f2e7b14dca08f3ebde21
SergioLopezMaldonado <sergiolopezmaldonado50@outlook.com>
2025-10-04 19:33:41 -0600

    hover Blue encima

--- src/player.py
+++ src/player.py
@@ -14,11 +14,20 @@ class Player():
         self.grid = {}
         self.camera = Camera()
         self.previsualization_block = Previsualization_Block()
-
     def update_player_grid_position(self):
-    #Actualiza la posicion del jugador en el grid
-        self.gridPosition  =  self.camera.iso_to_grid(self.isoPosition)
-        self.previsualization_block.update_position_block(self.gridPosition, self.camera )
+        """Actualiza la posición del jugador en el grid y la altura de previsualización."""
+        # Calcular la posición actual en el grid
+        self.gridPosition = list(self.camera.iso_to_grid(self.isoPosition))
+
+        x, y, z = self.gridPosition
+
+        # Buscar la primera altura libre (z más alto + 1)
+        while (x, y, z) in self.grid:
+            z += 1
+
+        # Actualizar posición del bloque de previsualización justo arriba
+        self.previsualization_block.update_position_block((x, y, z), self.camera)
+
 
     def add_block(self, img, tipo="default"):
         x, y, z = self.gridPosition
@@ -27,13 +36,13 @@ class Player():
         if (x, y, z) not in self.grid:
          
             self.grid[(x, y, z)] = {"tipo": tipo, "img": img}
-            self.previsualization_block.update_block_image(img)
             self.layout.add(Block((x, y, z), img), layer=x + y + z)
 
         # Si ya hay bloque, apilar hacia arriba
         else:
             while (x, y, z) in self.grid:
                 z += 1
+            self.previsualization_block.update_position_block((x, y, z), self.camera )
             self.grid[(x, y, z)] = {"tipo": tipo, "img": img}
             self.previsualization_block.update_block_image(img)
             self.layout.add(Block((x, y, z), img), layer=x + y + z)
